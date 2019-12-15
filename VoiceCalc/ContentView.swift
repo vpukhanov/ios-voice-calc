ae3ef4872cf6855f394b79968e3f00c513a436e8
Vyacheslav Pukhanov <vyacheslav.pukhanov@gmail.com>
2019-12-15 08:57:36 +0300

    Initial Commit

---
+++ VoiceCalc/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VoiceCalc
+//
+//  Created by Вячеслав Пуханов on 15.12.2019.
+//  Copyright © 2019 Вячеслав Пуханов. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
